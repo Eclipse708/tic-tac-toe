496ffcce7e6a1c02f69a34aef65a6bec0ba6d785
eclipse708 <asfand606@gmail.com>
2024-10-06 00:23:52 +0500

    initial functionality

---
+++ script.js
@@ -0,0 +1,15 @@
+const Gameboard = () => {
+    const board = Array(9).fill(null);
+
+    
+    const updateBoard = (position, marker) => {
+        board.forEach((cell) => {
+            console.log(cell);
+        });
+    }
+    
+}
+
+// const game = Gameboard();
+// game;
+
